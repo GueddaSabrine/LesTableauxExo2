34f87bd072bc615e418ca46cfd4d9ddff48de208
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-19 10:14:35 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LesTableauxExo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Créer une liste languages contenant les éléments suivants :
+            //HTML
+            //CSS
+            //Javacrit
+            //jQuery
+            //PHP
+            //Bootstrap
+            //Java
+            //Afficher toute la liste.
+            //Afficher le 4ème élément de la liste.
+            //Afficher l’élément à l’index 4 de la liste.
+            //Supprimer Bootstrap de la liste.
+            //Modifier Javacrit en Javascript.
+            //Ajouter C dans la liste.
+            //Afficher la liste.
+
+            // Create a list of parts.
+            //List<Part> parts = new List<Part>();
+            //List<string> AuthorList = new List<string>();
+
+            /*List<string> languages = new List<string>();
+            string[] language = {"HTML", "CSS", "Javacrit", "jQuery", "PHP", "Bootstrap", "Java"};
+            Console.WriteLine("Affichage de la liste de départ: " + languages);*/
+
+            List<string> languages = new List<string> {"HTML", "CSS", "Javacrit", "jQuery", "PHP", "Bootstrap", "Java"};
+            Console.WriteLine("liste compléte des différents langauges : ");
+
+            for (int i = 0; i < languages.Count; i++)
+
+            {
+                Console.WriteLine(languages[i]);
+            }
+
+            Console.WriteLine("4e élément de la liste : " + languages[3]); //4e élément donc index 3
+            Console.WriteLine("index 4 de la liste : " + languages[4]); //index 4
+            languages.Remove("Bootstrap");
+            languages[2] = "Javascript";
+            languages.Add("C");
+            Console.WriteLine("Liste modifier : ");
+            for (int i = 0; i < languages.Count; i++)
+
+            {
+                Console.WriteLine(languages[i]);
+
+            }
+            
+
+
+
+        }
+    }
+}
